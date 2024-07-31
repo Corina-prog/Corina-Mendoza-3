9ede0eba5cdf413c8d079d6b33e97150facbb1ef
Corina Mendoza <vivianmendoza07@hotmail.com>
2024-07-31 07:57:29 -0600

    mi examen

---
+++ src/examen/Examen.java
@@ -0,0 +1,28 @@
+
+package examen;
+
+public class Examen {
+
+    public static void main(String[] args) {
+   int miInicial [][]= {{10, 20, 30, 40}, {1, 2, 3, 4}, {20, 21, 22, 23}};
+   int Mcopia [][] = new int[3][4];
+   int fila =2;
+   int col = 2;
+   
+        for (int i = 0; i <miInicial.length; i++) {
+            for (int j = 0; j < miInicial[0].length; j++) {
+               Mcopia[fila][col] = miInicial[i][j];   
+             col--;   
+            }
+            fila--;
+            col =2;
+        }
+         for (int i = 0; i <miInicial.length; i++) {
+            for (int j = 0; j < miInicial[0].length; j++) {
+                System.out.print(Mcopia [i][j] + "|");
+            }
+             System.out.println("");
+    }
+    
+    }
+}
